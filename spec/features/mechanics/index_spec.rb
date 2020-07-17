540f9e65897c6ee23d092a1ebccf01e67557a7fb
Cam C <ckc.cameron@gmail.com>
2020-07-17 10:51:36 -0600

    Create mechanics index view page

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "mechanics index page" do
   before :each do
-    @mechanic_1 = Mechanic.create(name: "Sam Mills", years_of_experience: 10)
-    @mechanic_2 = Mechanic.create(name: "Kara Smith", years_of_experience: 11)
+    @mechanic_1 = Mechanic.create!(name: "Sam Mills", years_of_experience: 10)
+    @mechanic_2 = Mechanic.create!(name: "Kara Smith", years_of_experience: 11)
   end
 
   it "displays all mechanics" do
     visit "/mechanics"
     expect(page).to have_content("All Mechanics")
 
-    within ".mechanic-#{@mechanic_1.id}" do
+    within "#mechanics-#{@mechanic_1.id}" do
       expect(page).to have_content("Sam Mills")
       expect(page).to have_content("10 years of experience")
     end
 
-    within ".mechanic-#{@mechanic_2.id}" do
+    within "#mechanics-#{@mechanic_2.id}" do
       expect(page).to have_content("Kara Smith")
       expect(page).to have_content("11 years of experience")
     end
