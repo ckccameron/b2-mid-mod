26ea3db4bb73fa2d46c71fe076525f8c6f7b996f
Cam C <ckc.cameron@gmail.com>
2020-07-17 11:19:41 -0600

    Write spec for amusement parks sort rides alphabetically method

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe AmusementPark do
   describe "relationships" do
     it {should have_many :rides}
   end
+
+  describe "methods" do
+    it "#sort_rides_alphabetically" do
+      hershey_park = AmusementPark.create!(name: "Hershey Park",
+                                            admission_price: "$50")
+
+      ride_1 = hershey_park.rides.create!(name: "Lightning Racer",
+                                            thrill_rating: 7)
+      ride_2 = hershey_park.rides.create!(name: "Storm Runner",
+                                            thrill_rating: 9)
+      ride_3 = hershey_park.rides.create!(name: "The Great Bear",
+                                            thrill_rating: 7)
+
+      expect(hershey_park.sort_rides_alphabetically).to eq([ride_1, ride_2, ride_3])
+    end
+  end
 end
