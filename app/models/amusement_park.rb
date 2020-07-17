6bc6f4528e3000450c3bcfc1d5ec4663a78feb0c
Cam C <ckc.cameron@gmail.com>
2020-07-17 11:21:36 -0600

    Add sort rides alphabetically method to amusement parks model

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,5 +1,9 @@
 class AmusementPark < ApplicationRecord
   validates_presence_of :name, :admission_price
-  
+
   has_many :rides
+
+  def sort_rides_alphabetically
+    rides.order(:name)
+  end
 end
