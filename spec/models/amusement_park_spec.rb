ba52b6bc1fe8307bf4cc8974a8340100e1c7f4cc
Cam C <ckc.cameron@gmail.com>
2020-07-17 11:24:09 -0600

    Write spec for amusement park rides avg thrill rating method

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -19,5 +19,19 @@ RSpec.describe AmusementPark do
 
       expect(hershey_park.sort_rides_alphabetically).to eq([ride_1, ride_2, ride_3])
     end
+
+    it "#avg_thrill_rating" do
+      hershey_park = AmusementPark.create!(name: "Hershey Park",
+                                            admission_price: "$50")
+
+      ride_1 = hershey_park.rides.create!(name: "Lightning Racer",
+                                            thrill_rating: 7)
+      ride_2 = hershey_park.rides.create!(name: "Storm Runner",
+                                            thrill_rating: 9)
+      ride_3 = hershey_park.rides.create!(name: "The Great Bear",
+                                            thrill_rating: 7)
+
+      expect(hershey_park.avg_thrill_rating).to eq(7.7)
+    end
   end
 end
