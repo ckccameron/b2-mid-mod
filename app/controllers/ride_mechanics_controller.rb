19883417f1ca89b49dd4ae7b7e3805e20ddf02d8
Cam C <ckc.cameron@gmail.com>
2020-07-20 15:58:20 -0600

    Edit ride mechanics controller create to optimize db performance

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -1,9 +1,15 @@
 class RideMechanicsController < ApplicationController
   def create
-    ride = Ride.find(params[:ride_id])
-    mechanic = Mechanic.find(params[:mechanic_id])
-    ride_mechanic = RideMechanic.create(ride: ride, mechanic: mechanic)
-
+    # ride = Ride.find(params[:ride_id])
+    # mechanic = Mechanic.find(params[:mechanic_id])
+    # ride_mechanic = RideMechanic.create(ride: ride, mechanic: mechanic)
+    RideMechanic.create(ride_mechanic_params)
     redirect_to "/mechanics/#{params[:mechanic_id]}"
+  end
+
+  private
+
+  def ride_mechanic_params
+    params.permit(:ride_id, :mechanic_id)
   end
 end
